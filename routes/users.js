8e7d32a6d2dbac7ffb436a160d8e8a028cfe1f7f
曹珂俭 <1849201815@qq.com>
2023-07-27 23:46:02 +0800

    下发jwt并校验code

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const UserModel = require('../model/user');
 const Email = require('../mailer/index');
 const rateLimit = require('../mailer/utils');
 const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const uuid = require('../secret.key')
 
 // 定义数据验证规则
 const createUserValidationRules = [
@@ -42,15 +44,17 @@ router.post('/', createUserValidationRules, validate, async (req, res) => {
   }
 });
 
-
+let randomCode; // 保存验证码
+let stuid
 router.post('/email', async (req, res, next) => {
   const { stuId, email } = req.body
+  stuid = stuId
   const data = await UserModel.find({
     stuId
   })
-  if(data.length!==0){
-    if(data[0].email !== email || data[0].stuId !== +stuId){
-      return res.status(421).json({message:'学号或邮箱不属于本人！'})
+  if (data.length !== 0) {
+    if (data[0].email !== email || data[0].stuId !== +stuId) {
+      return res.status(421).json({ message: '学号或邮箱不属于本人！' })
     }
   }
   if (data.length !== 0) {
@@ -60,8 +64,8 @@ router.post('/email', async (req, res, next) => {
     if (!email) {
       return res.status(402).json(data(402, {}, "邮件格式错误"))
     }
-    const code = parseInt(Math.random() * 1000000) //生成随机验证码
-    Email.sendMail(email, code, (state) => {
+    randomCode = parseInt(Math.random() * 1000000) //生成随机验证码
+    Email.sendMail(email, randomCode, (state) => {
       if (state) {
         return res.status(200).json(data(200, {}))
       } else {
@@ -69,9 +73,32 @@ router.post('/email', async (req, res, next) => {
       }
     })
   } else {
-    return res.status(420).json({message: '服务器出错，联系工作人员！'})
+    return res.status(420).json({ message: '服务器出错，联系工作人员！' })
   }
-
 });
 
+router.post('/veifycode', async (req, res, next) => {
+  const { code } = req.body
+  if (!code) {
+    return res.status(400).send({code:400, message: '请输入验证码！' });
+  }
+  // 比较用户输入的验证码与之前保存的随机验证码
+  if (parseInt(code) === randomCode) {
+     // 验证码输入正确，生成 JWT
+     const secretKey = uuid; // 请替换为您的实际密钥
+     const payload = { stuId: stuid }; // 可根据需要设置 JWT 的 payload
+     const options = { expiresIn: '2h' }; // 设置 JWT 的有效期，这里设置为 1 小时
+     jwt.sign(payload, secretKey, options, (err, token) => {
+       if (err) {
+         console.error('生成 JWT 出错：', err);
+         return res.status(500).json({ code: 500, message: '生成 JWT 出错' });
+       }
+       // 成功生成 JWT，将 JWT 返回给客户端
+       res.setHeader('Authorization', `Bearer ${token}`);
+       return res.status(200).json({ message: '验证码输入正确！'});
+     });
+  } else {
+    return res.status(401).send({code:401,message:'验证码错误！'});
+  }
+})
 module.exports = router;
---
+++ secret.key.js
@@ -0,0 +1,3 @@
+ const uuid = '6fc4e42b-7b3d-4fb8-b4de-8d29b3e076a3'
+
+ module.exports = uuid
