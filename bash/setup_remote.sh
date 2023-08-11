d79ff96ec4eab7d50b0c1e3759f229fe0e7da0a4
曹珂俭 <1849201815@qq.com>
2023-08-11 14:20:41 +0800

    feat(): 更新自动化脚本

--- bash/setup_remote.sh
+++ bash/setup_remote.sh
@@ -21,8 +21,8 @@ title "服务端开始执行脚本"
 title "进入目录"
 cd $deploy_dir/
 title "下载依赖"
-sudo npm install 
+sudo npm install --progress
 title "启动服务"
 killall node
-npm run dev &
+npm run dev > /dev/null 2>&1 &
 title '执行完毕！请访问:"http://43.139.142.203:3000/api"'
