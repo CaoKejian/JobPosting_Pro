42cb4c4457b660551534e470c5cef939336c8856
曹珂俭 <1849201815@qq.com>
2023-08-11 14:46:07 +0800

    feat(): 更新脚本设置环境变量

--- bash/setup_remote.sh
+++ bash/setup_remote.sh
@@ -18,11 +18,9 @@ cd $deploy_dir/db/ && sudo rm -rf *.json
 mongorestore -d $db_name $db_folder
 echo "迁移完成！"
 title "服务端开始执行脚本"
-title "进入目录"
-cd $deploy_dir/
 title "下载依赖"
-sudo npm install --progress
+sudo npm install
 title "启动服务"
 killall node
-npm run dev > /dev/null 2>&1 &
+export NODE_ENV=production && npm run dev:pro > /dev/null 2>&1 &
 title '执行完毕！请访问:"http://43.139.142.203:3000/api"'
