fba6c4f88060c3ef00f632fc7eeb4786b421ce1b
曹珂俭 <1849201815@qq.com>
2023-08-18 18:57:12 +0800

    test: 添加按学科查询测试用例

--- rspec/test/publish.rspec.js
+++ rspec/test/publish.rspec.js
@@ -70,4 +70,23 @@ describe('作业发布', function () {
     expect(data).to.have.lengthOf(1)
     expect(data[0].user).to.equal('xxx')
   })
+  it('按学科查询',async function (){
+    const testData = {
+      user: 'xxx',
+      classId: 123123,
+      subject: '测试',
+      branch: '积分',
+      content: '预习积分，完成第二章课后题T32.',
+      cutTime: 120
+    }
+    await PublishWorkModel.create(testData)
+    const res = await request(app)
+      .get('/api/pub/subject')
+      .query({subject: '测试'})
+      .expect(200)
+    const data = res.body
+    expect(data).to.be.an('array')
+    expect(data).to.have.lengthOf(1)
+    expect(data[0].subject).to.equal('测试')
+  })
 })
