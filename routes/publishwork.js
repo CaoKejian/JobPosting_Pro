ef3c3cbd2320e023a8023c7241a930c7a1bacfa2
曹珂俭 <1849201815@qq.com>
2023-08-18 16:42:26 +0800

    feat: 完善发布作业接口

--- routes/publishwork.js
+++ routes/publishwork.js
@@ -5,21 +5,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  const { body } = req.query
+  const { user,
+    classId,
+    subject,
+    branch,
+    cutTime,
+    content, } = req.query
   codeTimestamp = Date.now();
   const twoDaysInMillis = 2 * 24 * 60 * 60 * 1000; 
-  const endTime = codeTimestamp + twoDaysInMillis;
-  const data = await PublishWorkModel.create(body)
-  // const data = await PublishWorkModel.create({
-  //   user: '嘿老师',
-  //   classId: 123123,
-  //   subject: '高数二',
-  //   branch: '积分',
-  //   time: codeTimestamp,
-  //   cutTime: endTime, //截止时间
-  //   content: '预习积分，完成第二章课后题T32.',
-  // })
+  // const cutTime = codeTimestamp + twoDaysInMillis;
+  const data = await PublishWorkModel.create({
+    user,
+    classId,
+    subject,
+    branch,
+    time: codeTimestamp,
+    cutTime, //截止时间
+    content,
+  })
   res.send(data)
 });
+
 
 module.exports = router;
