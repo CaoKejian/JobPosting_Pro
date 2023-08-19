98256dca0fb7c97669597f434c35a3df0684e3e9
曹珂俭 <1849201815@qq.com>
2023-08-19 13:59:32 +0800

    test: 更新subject测试用例

--- rspec/test/subject.rspec.js
+++ rspec/test/subject.rspec.js
@@ -11,9 +11,10 @@ describe('学科', function () {
   it('查询发布成功', async function () {
     const classId = 123123
     const subject = 'React'
+    const user = '曹Sir'
     const res = await request(app)
-      .get('/api/subject')
-      .query({ classId, subject })
+      .post('/api/subject')
+      .send({ classId, subject, user })
       .expect(200)
     const data = res.body
     expect(data.subject).to.equal('React')
@@ -21,14 +22,16 @@ describe('学科', function () {
   it('查询重复发布', async function () {
     const testData = {
       classId: 123123,
-      subject: '高数'
+      subject: '高数',
+      user: '曹Sir'
     }
     await SubjectModel.create(testData)
     const classId = 123123
     const subject = '高数'
+    const user = '曹Sir'
     const res = await request(app)
-    .get('/api/subject')
-    .query({classId, subject})
+    .post('/api/subject')
+    .send({classId, subject, user})
     .expect(402)
     const data = res.body
     expect(data.message).to.equal('已存在该学科！')
