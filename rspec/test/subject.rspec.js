0fdf67291365349c19fabac05bde82c80fc9d180
曹珂俭 <1849201815@qq.com>
2023-08-22 14:38:40 +0800

    test: subject添加测试用例

--- rspec/test/subject.rspec.js
+++ rspec/test/subject.rspec.js
@@ -30,10 +30,29 @@ describe('学科', function () {
     const subject = '高数'
     const user = '曹Sir'
     const res = await request(app)
-    .post('/api/subject')
-    .send({classId, subject, user})
-    .expect(402)
+      .post('/api/subject')
+      .send({ classId, subject, user })
+      .expect(402)
     const data = res.body
     expect(data.message).to.equal('已存在该学科！')
+  })
+  it('根据作者查询所有班级和所有学科', async function () {
+    const testData = [
+      { classId: 123123, subject: '高数', user: '曹Sir' },
+      { classId: 122122, subject: 'React', user: '曹Sir' },
+      { classId: 123123, subject: '英语', user: '曹Sir' },
+      { classId: 123123, subject: '英语', user: '曹Sir222' },
+    ]
+    await SubjectModel.insertMany(testData)
+    const user = '曹Sir'
+    const res = await request(app)
+      .get('/api/subject/myclass')
+      .query({ user })
+      .expect(200)
+    const data = res.body
+    expect(data).to.be.an('object')
+    expect(data.classes).to.be.an('array')
+    expect(data.classes).to.have.lengthOf(2)
+    expect(data.subjects).to.have.lengthOf(3)
   })
 })
