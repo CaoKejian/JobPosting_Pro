d859fc481c067c09210af33d057ec9b6011f9852
曹珂俭 <1849201815@qq.com>
2023-08-23 09:58:10 +0800

    feat：查询班级下所有作业

--- routes/homeWork.js
+++ routes/homeWork.js
@@ -71,6 +71,27 @@ router.get('/mywork', async function (req, res) {
 })
 
 /** 
+  * @param {classId}
+  * @method 查询近30天该班级下的所有提交作业
+  */
+
+router.get('/classwork', async function (req, res) {
+  const { classId } = req.query
+  // 获取当前日期的时间戳
+  const currentDate = new Date();
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(currentDate.getDate() - 30);
+  const data = await HomeWorkModel.find({
+    classId,
+    timestamp: { $gte: thirtyDaysAgo.getTime() } // 使用$gte操作符来匹配大于等于指定时间戳的文档
+  });
+  if(data.length===0){
+    res.status(402).json({message: '没有相关数据！'})
+  }
+  res.send(data)
+})
+
+/** 
   * @param {id}
   * @method 查询_id的作业
   */
@@ -109,17 +130,39 @@ router.get('/one', async function (req, res) {
 
 /** 
   * @param {classId, branch}
-  * @method 查询某班级的某个作业分支所有提交的作业
+  * @method 查询单项作业
   */
 
 router.get('/correct/work', async function (req, res) {
   const { classId, branch } = req.query
   try {
-    const data = await HomeWorkModel.find({
+    const data = await HomeWorkModel.findOne({
       classId,
       branch
     })
     res.json(data)
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+})
+
+/** 
+  * @param {classId, branch}
+  * @method 查询多项作业
+  */
+
+router.get('/class/allWork', async function (req, res) {
+  const { classId, branch } = req.query
+  try {
+    const data = await HomeWorkModel.find({
+      classId,
+      branch
+    }).select('stuId classId')
+    if(data.length === 0){
+      res.status(402).json({message: '还没有人提交！'})
+    }else{
+      res.json(data)
+    }
   } catch (error) {
     res.status(500).json({ message: error })
   }
