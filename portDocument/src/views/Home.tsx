15645832d927051785ae1ad864377608137c2f93
曹珂俭 <1849201815@qq.com>
2023-08-24 14:23:00 +0800

    feat: 完善滚动切换

--- portDocument/src/views/Home.tsx
+++ portDocument/src/views/Home.tsx
@@ -1,15 +1,26 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
 import s from './Home.module.scss'
 import type { FC, ReactNode } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useScrollDown from '../hooks/useScroll'
 interface IProps {
   children?: ReactNode
 }
 
 const Home: FC<IProps> = () => {
+  const { canScrollUp: isMove } = useScrollDown(true,false)
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isMove) {
+      setTimeout(() => {
+          navigate('/welcome', {state: {message: 'home_return'}})
+      }, 200);
+    }
+  }, [isMove])
   return (
     <div className={s.wrapper}>
       <div className={s.left}>侧边栏</div>
-      <div className={s.right}></div>
+      <div className={s.right}>haha</div>
     </div>
   )
 }
--- portDocument/src/views/WelcomePage.tsx
+++ portDocument/src/views/WelcomePage.tsx
@@ -2,10 +2,9 @@
 
 import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import s from './Welcome.module.scss'
 import useScrollDown from '../hooks/useScroll';
-import { TransitionGroup, CSSTransition, SwitchTransition } from 'react-transition-group';
 import './transition.css'
 
 interface IProps {
@@ -13,8 +12,36 @@ interface IProps {
 }
 
 const WelcomePage: FC<IProps> = () => {
-  const isMove = useScrollDown()
+  const location = useLocation();
+  const [isReturn, setReturn] = useState(false)
+  useEffect(() => {
+    if (location.state) {
+      const message = location.state.message || 0
+      if (message === 'home_return') {
+        setReturn(true)
+      }
+    }
+  }, [])
+  const { canScrollDown: isMove } = useScrollDown(false,true)
   const navigate = useNavigate();
+  const Welcome = () => {
+    return (
+      <div className={s.active}>
+        <svg className={s.svg}><use xlinkHref='#welcome'></use></svg>
+        <p>欢迎来到</p>
+        <span>交作业啦App-接口文档</span>
+        <div className={s.user}>
+          <span>作者：<span className={s.colin}>Colin</span></span>
+          <span>前端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting'>代码链接</Link>访问地址：<Link target="_blank" to='http://jobpost-1314966552.cos-website.ap-shanghai.myqcloud.com/'>点击前往</Link></span>
+          <span>后端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro'>代码链接</Link>访问地址：<Link target="_blank" to='http://43.139.142.203:3000/api'>点击前往</Link></span>
+          <span>接口文档：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro/tree/main/portDocument'>代码链接</Link></span>
+        </div>
+        <div className={s.bottom}>
+          <svg className={s.tobottom}><use xlinkHref='#tobottom'></use></svg>
+        </div>
+      </div>
+    )
+  }
   useEffect(() => {
     if (isMove) {
       setTimeout(() => {
@@ -23,20 +50,23 @@ const WelcomePage: FC<IProps> = () => {
     }
   }, [isMove])
   return (
-    <div className={s.wrapper}>
-      <svg className={s.svg}><use xlinkHref='#welcome'></use></svg>
-      <p>欢迎来到</p>
-      <span>交作业啦App-接口文档</span>
-      <div className={s.user}>
-        <span>作者：<span className={s.colin}>Colin</span></span>
-        <span>前端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting'>代码链接</Link>访问地址：<Link target="_blank" to='http://jobpost-1314966552.cos-website.ap-shanghai.myqcloud.com/'>点击前往</Link></span>
-        <span>后端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro'>代码链接</Link>访问地址：<Link target="_blank" to='http://43.139.142.203:3000/api'>点击前往</Link></span>
-        <span>接口文档：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro/tree/main/portDocument'>代码链接</Link></span>
+    isReturn ? (
+      <Welcome />) : (
+      <div className={s.wrapper}>
+        <svg className={s.svg}><use xlinkHref='#welcome'></use></svg>
+        <p>欢迎来到</p>
+        <span>交作业啦App-接口文档</span>
+        <div className={s.user}>
+          <span>作者：<span className={s.colin}>Colin</span></span>
+          <span>前端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting'>代码链接</Link>访问地址：<Link target="_blank" to='http://jobpost-1314966552.cos-website.ap-shanghai.myqcloud.com/'>点击前往</Link></span>
+          <span>后端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro'>代码链接</Link>访问地址：<Link target="_blank" to='http://43.139.142.203:3000/api'>点击前往</Link></span>
+          <span>接口文档：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro/tree/main/portDocument'>代码链接</Link></span>
+        </div>
+        <div className={s.bottom}>
+          <svg className={s.tobottom}><use xlinkHref='#tobottom'></use></svg>
+        </div>
       </div>
-      <div className={s.bottom}>
-        <svg className={s.tobottom}><use xlinkHref='#tobottom'></use></svg>
-      </div>
-    </div>
+    )
   )
 }
 
