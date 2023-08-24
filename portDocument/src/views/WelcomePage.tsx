f5e7173762f7ff98365606a86564493aa84a8a75
曹珂俭 <1849201815@qq.com>
2023-08-24 09:43:24 +0800

    feat: 下滑事件监听

---
+++ portDocument/src/views/WelcomePage.tsx
@@ -0,0 +1,42 @@
+
+
+import React, { memo, useState } from 'react'
+import type { FC, ReactNode } from 'react'
+import { Link } from 'react-router-dom';
+import s from './Welcome.module.scss'
+
+interface IProps {
+  children?: ReactNode
+}
+
+const WelcomePage: FC<IProps> = () => {
+  const [canPrint, setCanPrint] = useState(true);
+  const onMove = (e: React.WheelEvent<HTMLDivElement>) => {
+    if (!canPrint) return
+    if (e.deltaY > 0) {
+      setCanPrint(false)
+      console.log('向下滚动')
+      setTimeout(() => {
+        setCanPrint(true)
+      }, 1000);
+    }
+  }
+  return (
+    <div className={s.wrapper} onWheel={onMove}>
+      <svg className={s.svg}><use xlinkHref='#welcome'></use></svg>
+      <p>欢迎来到</p>
+      <span>交作业啦App-接口文档</span>
+      <div className={s.user}>
+        <span>作者：<span className={s.colin}>Colin</span></span>
+        <span>前端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting'>代码链接</Link>访问地址：<Link target="_blank" to='http://jobpost-1314966552.cos-website.ap-shanghai.myqcloud.com/'>点击前往</Link></span>
+        <span>后端：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro'>代码链接</Link>访问地址：<Link target="_blank" to='http://43.139.142.203:3000/api'>点击前往</Link></span>
+        <span>接口文档：<Link target="_blank" to='https://github.com/CaoKejian/JobPosting_Pro/tree/main/portDocument'>代码链接</Link></span>
+      </div>
+      <div className={s.bottom}>
+        <svg className={s.tobottom}><use xlinkHref='#tobottom'></use></svg>
+      </div>
+    </div>
+  )
+}
+
+export default memo(WelcomePage)
