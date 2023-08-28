ae6ed8cb82601f2c0c94fc9ae210f527daa2e047
曹珂俭 <1849201815@qq.com>
2023-08-28 14:09:32 +0800

    test: 设置权限测试用例

--- rspec/test/user.rspec.js
+++ rspec/test/user.rspec.js
@@ -25,4 +25,18 @@ describe('user', function(req,res){
     expect(data).to.have.lengthOf(2)
     expect(data[0].name).to.equal('1')
   })
+  it('设置权限', async function(){
+    const textData = [
+      {classId:1, name:'1',isAuth:false,stuId:111},
+      {classId:1, name:'2',isAuth:false,stuId:222},
+      {classId:1, name:'3',isAuth:false,stuId:333},
+    ]
+    await UserModel.insertMany(textData)
+    const res = await request(app)
+      .post('/api/user/president/set')
+      .send({ stuId:123123 })
+      .expect(200);
+    const data = res.body
+    expect(data.data).to.equal(true)
+  })
 })
