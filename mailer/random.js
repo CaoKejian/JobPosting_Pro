ac749afd4197045b5fa117444d93ff569a33376a
曹珂俭 <1849201815@qq.com>
2023-08-29 21:20:08 +0800

    readme: 文档更新

--- mailer/random.js
+++
@@ -1,4 +0,0 @@
-var code_fill_str = ["000000", "00000", "0000", "000", "00", "0", ""];
-var code = '' + parseInt(Math.random() * 1000000);
-code = code_fill_str[code.length] + code;
-module.exports = code;
--- secret.key.js
+++
@@ -1,3 +0,0 @@
- const uuid = '6fc4e42b-7b3d-4fb8-b4de-8d29b3e076a3'
-
- module.exports = uuid
