b3a01f9b69557f9a575d1624d6f30c7e7b070fb5
caokejian_i <caokejian_i@didiglobal.com>
2024-02-18 16:59:09 +0800

    config: set

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,8 @@ mongoose.set('strictQuery', true);
 mongoose.connect(`mongodb://127.0.0.1:27017/${tableName}`)
 
 mongoose.connection.on("open", () => {
-  console.log('\x1b[33m%s\x1b[0m', '数据库 已成功连接！') 
+  console.log('\x1b[33m%s\x1b[0m', '数据库 已成功连接！')
+  console.log('\x1b[33m%s\x1b[0m', `当前连接表：${tableName}`)
   // 在这里可以执行其他操作
 });
 
